054c7e8380c823ad84caf6acfaa4a66baa694acd
laizuakram1 <laizuakram98@gmail.com>
2022-03-30 00:22:15 +0600

    add handler in nav icon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLink from '../NavLink/NavLink';
-import { MenuIcon } from '@heroicons/react/solid'
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     const routes = [
         {id: 1, name:'Home', link:'https:www.google.com'},
         {id: 2, name:'SHOP', link:'https:www.google.com'},
@@ -13,11 +15,11 @@ const Navbar = () => {
     ]
     return (
         <nav >
-            <div className='w-6 h-6 md:hidden'>
-                <MenuIcon></MenuIcon>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+              {open ? <XIcon></XIcon>: <MenuIcon></MenuIcon>}
             </div>
             
-            <ul className='md:flex justify-center'>
+            <ul className={`md:flex justify-center absolute duration-500 ${open ? 'top-6' : 'top-[-120px]'}`}>
                {
                    routes.map(route => <NavLink 
                    key = {route.id}
